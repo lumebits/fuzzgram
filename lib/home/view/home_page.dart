c65797b0f91c3fe79e2e99906ddae8a92cb7ef53
Paloma Piot <piot.paloma@gmail.com>
2021-02-17 20:36:51 +0100

    home templates

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -12,12 +12,30 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => HomeBloc(templateRepository: FirebaseTemplateRepository())..add(FetchTemplates()),
-      child: HomeList(),
+      child: ScrollableHomePage(),
     );
   }
 }
 
-class HomeList extends StatelessWidget {
+class ScrollableHomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+
+}
+
+class _HomePageState extends State<ScrollableHomePage> {
+  final _scrollController = ScrollController();
+  final _scrollThreshold = 200.0;
+  HomeBloc _homeBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+    _homeBloc = BlocProvider.of<HomeBloc>(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<HomeBloc, HomeState>(builder: (context, state) {
@@ -25,15 +43,23 @@ class HomeList extends StatelessWidget {
         return Center(child: CircularProgressIndicator());
       } else if (state is HomeSuccess) {
         final templatesList = state.templates;
-        return SafeArea(
-          child: ListView.builder(
-            itemCount: templatesList.length,
+        return Expanded(
+          child: GridView.builder(
+            padding: EdgeInsets.only(top: 75.0),
+            controller: _scrollController,
+            shrinkWrap: true,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: MediaQuery.of(context).size.width /
+                  (MediaQuery.of(context).size.height),
+            ),
+            itemCount: state.hasReachedMax ? templatesList.length : templatesList.length + 1,
             itemBuilder: (context, index) {
               final template = templatesList[index];
-              return Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: TemplateItem(template: template),
-              );
+              return index >= templatesList.length ? BottomLoader() :
+                GridTile(
+                  child: TemplateWidget(template: template)
+                );
             },
           ),
         );
@@ -42,25 +68,66 @@ class HomeList extends StatelessWidget {
       }
     });
   }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.position.pixels;
+    if (maxScroll - currentScroll <= _scrollThreshold) {
+      _homeBloc.add(FetchTemplates());
+    }
+  }
+
 }
 
-class TemplateItem extends StatelessWidget {
+class TemplateWidget extends StatelessWidget {
   final Template template;
 
-  TemplateItem({Key key, @required this.template}) : super(key: key);
+  TemplateWidget({Key key, @required this.template}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ExpansionTile(
-      key: new PageStorageKey<String>(template.id),
-      title: Text(template.name),
-      leading: Icon(
-        Icons.ac_unit,
-        color: Colors.green,
+    return InkWell(
+        borderRadius: BorderRadius.circular(10.0),
+        onTap: () {
+          print("Template tapped: " + template.id + " " + template.name);
+        },
+        child: Card(
+          semanticContainer: true,
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          child: Image.network(
+            template.imageUrl,
+            fit: BoxFit.fill,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          elevation: 5,
+          margin: EdgeInsets.all(10),
+        )
+    );
+  }
+}
+
+class BottomLoader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Center(
+        child: SizedBox(
+          width: 33,
+          height: 33,
+          child: CircularProgressIndicator(
+            strokeWidth: 1.5,
+          ),
+        ),
       ),
-      children: [
-        Center()
-      ],
     );
   }
 }
